3127a7af812281bad682b46efcdf8961abbfc669
Emma Dedmond <emmadedmond@yahoo.com>
2019-03-09 13:17:44 -0500

    streamlined some code

--- routes/express.js
+++ routes/express.js
@@ -15,26 +15,6 @@ app.use(express.json());
 tables.push({name: "bob", 'phone-number': "9999",  email: "ew@dfs"})
 waiting.push({ name: "Sue", 'phone-number': "123123123", email: "suesemail@dfs" })
 
-// app.get('/:thisRoute', function(req, res) {
-//     const page = path.join(__dirname, req.params.thisRoute);
-//     console.log(page);
-//     // let pathName = path.join(__dirname,"../public/home.html");
-//     // console.log(pathName);
-//     switch (thisRoute) {
-//         case "home.html":
-//             break;
-//         case "reserve.html":
-//             break;
-//         case "tables.html":
-//             break;
-//         default:
-//             console.log(thisRoute + " not found")
-//     }
-
-
-//     res.sendFile(path.join(__dirname,"../public/home.html"));
-// });
-
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname,"../public/home.html"));
 })
@@ -56,20 +36,6 @@ app.get("/api/waitlist", function (req, res) {
 // Displays reserved tables
 app.get("/api/tables", function(req, res) {
     return res.json(tables);
-  });
+});
 
-// Displays a single table, or returns false
-app.get("/api/tables/:table", function(req, res) {
-    const reservedTable = req.params.table;
-  
-    console.log(reservedTable);
-  
-    for (let i = 0; i < tables.length; i++) {
-      if (reservedTable === tables[i].pathName) {
-        return res.json(tables[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
- 
+
