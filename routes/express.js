00c35e08cbcf5401be6aebac3a1b47ea3d828bd7
Emma Dedmond <emmadedmond@yahoo.com>
2019-03-09 12:43:16 -0500

    working redirect for web; not api

--- routes/express.js
+++ routes/express.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const path = require("path");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const tables = [];
 const waiting = [];
@@ -15,30 +15,32 @@ app.use(express.json());
 tables.push({name: "bob", 'phone-number': "9999",  email: "ew@dfs"})
 waiting.push({ name: "Sue", 'phone-number': "123123123", email: "suesemail@dfs" })
 
-app.get('/:thisRoute', function(req, res) {
-    const page = path.join(__dirname, req.params.thisRoute);
-    console.log(page);
-    // let pathName = path.join(__dirname,"../public/home.html");
-    // console.log(pathName);
-    switch (thisRoute) {
-        case "home.html":
-            break;
-        case "reserve.html":
-            break;
-        case "tables.html":
-            break;
-        default:
-            console.log(thisRoute + " not found")
-    }
+// app.get('/:thisRoute', function(req, res) {
+//     const page = path.join(__dirname, req.params.thisRoute);
+//     console.log(page);
+//     // let pathName = path.join(__dirname,"../public/home.html");
+//     // console.log(pathName);
+//     switch (thisRoute) {
+//         case "home.html":
+//             break;
+//         case "reserve.html":
+//             break;
+//         case "tables.html":
+//             break;
+//         default:
+//             console.log(thisRoute + " not found")
+//     }
 
 
+//     res.sendFile(path.join(__dirname,"../public/home.html"));
+// });
+
+app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname,"../public/home.html"));
-});
-
-
+})
 app.get('/:myRoute', function (req, res) {
     const page = req.params.myRoute;
-    res.sendFile(path.join(__dirname, page + '.html'));
+    res.sendFile(path.join(__dirname, "../public/" +page + '.html'));
     console.log("you hit " + page);
 });
 
